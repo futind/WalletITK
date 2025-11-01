ddbf8b9554d1f185b84f06f19e897c3098f02e23
Daniil <futinde@gmail.com>
2025-11-01 17:59:07 +0300

    Added exception handling

---
+++ src/main/java/ru/itk/wallet/balance/exception/ErrorResponseFactory.java
@@ -0,0 +1,32 @@
+package ru.itk.wallet.balance.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.ErrorResponse;
+import org.springframework.web.context.request.WebRequest;
+
+import java.net.URI;
+
+public class ErrorResponseFactory {
+
+    /**
+     * Method which is used in order to construct an error response
+     * @param status - HTTP status of an exception
+     * @param ex - exception which caused the creation of this message
+     * @param request - the request in which the exception had been raised
+     * @return {@link ErrorResponse} - an RFC 9457 compliant error message wrapped into {@link ResponseEntity} for convenience.
+     */
+    public static ResponseEntity<ErrorResponse> create(HttpStatus status, Throwable ex, WebRequest request) {
+        String path = request.getDescription(false).replaceFirst("^uri=", "");
+        String type = (path + '/' +  ex.getClass().getSimpleName().toLowerCase());
+
+        return ResponseEntity
+                .status(status)
+                .body(
+                        ErrorResponse.builder(ex, status, ex.getMessage())
+                                .type(URI.create(type))
+                                .instance(URI.create(path))
+                                .build()
+                );
+    }
+}
---
+++ src/main/java/ru/itk/wallet/balance/exception/WalletBalanceExceptionHandler.java
@@ -0,0 +1,40 @@
+package ru.itk.wallet.balance.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.ErrorResponse;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+import ru.itk.wallet.balance.controller.WalletBalanceController;
+import ru.itk.wallet.utils.exception.WalletNotFoundException;
+
+/**
+ * Exception handler class for WalletBalanceController
+ */
+@ControllerAdvice(assignableTypes = {WalletBalanceController.class})
+public class WalletBalanceExceptionHandler {
+
+    /**
+     * A method which handles NotFound exceptions
+     * @param ex - the exception raised
+     * @param request - request in which the exception had been raised
+     * @return RFC 9457 {@link ErrorResponse} wrapped into {@link ResponseEntity}
+     */
+    @ExceptionHandler(value = {WalletNotFoundException.class})
+    public ResponseEntity<ErrorResponse> handleWalletNotFoundException(WalletNotFoundException ex, WebRequest request) {
+        return ErrorResponseFactory.create(HttpStatus.NOT_FOUND, ex, request);
+    }
+
+    /**
+     * A method which handles validation exception (type mismatch)
+     * @param ex - the exception raised
+     * @param request - request in which the exception had been raised
+     * @return RFC 9457 {@link ErrorResponse} wrapped into {@link ResponseEntity}
+     */
+    @ExceptionHandler(value = {MethodArgumentTypeMismatchException.class})
+    public ResponseEntity<ErrorResponse> handleTypeMismatchException(MethodArgumentTypeMismatchException ex, WebRequest request) {
+        return ErrorResponseFactory.create(HttpStatus.BAD_REQUEST, ex, request);
+    }
+}
