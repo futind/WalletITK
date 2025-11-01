fb1920229f48d2f75f831adc0b9fbb2e40bb6e6a
Daniil <futinde@gmail.com>
2025-11-01 18:04:48 +0300

    Added basic functionality to the controller

--- src/main/java/ru/itk/wallet/balance/controller/WalletBalanceController.java
+++ src/main/java/ru/itk/wallet/balance/controller/WalletBalanceController.java
@@ -1,24 +1,38 @@
 package ru.itk.wallet.balance.controller;
 
+import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import ru.itk.wallet.balance.api.WalletBalanceAPI;
 import ru.itk.wallet.balance.dto.WalletBalanceResponse;
+import ru.itk.wallet.balance.service.WalletBalanceService;
 
 import java.util.UUID;
 
-@RestController
+/**
+ * REST controller for "view balance" feature
+ */
+@RestController("/api/v1")
 public class WalletBalanceController implements WalletBalanceAPI {
+
+    private final WalletBalanceService balanceService;
+
+    public WalletBalanceController(WalletBalanceService balanceService) {
+        this.balanceService = balanceService;
+    }
 
     /**
      * GET(/api/v1/wallets/{wallet_uuid}
      * A method used in order to check the balance of a particular wallet
      *
-     * @param walletId - unique identifier ({@link UUID}) of wallet
+     * @param wallet_uuid - unique identifier ({@link UUID}) of wallet
      * @return {@link WalletBalanceResponse} - information about wallet's balance
      */
     @Override
-    public ResponseEntity<WalletBalanceResponse> getBalance(UUID walletId) {
-        return null;
+    @GetMapping("/wallets/{wallet_uuid}")
+    public ResponseEntity<WalletBalanceResponse> getBalance(@PathVariable @Valid UUID wallet_uuid) {
+        return ResponseEntity.ok().body(balanceService.getBalance(wallet_uuid));
     }
 }
